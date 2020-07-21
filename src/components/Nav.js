5c84b52f4088270a8bdd57d6e9d37cb97f3b7977
Jantzen-E <jantzen.egan@gmail.com>
2020-07-20 23:58:40 -0600

    add redirect to build folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, NavLink, Redirect } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default class Nav extends Component {
   render() {
